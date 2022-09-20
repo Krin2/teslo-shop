5a7d15452d2e7e099582f17090e76c2967fa9a4b
Marcos Oscar Frigerio <marcos.frigerio@intive.com>
2022-09-20 16:39:12 -0300

    Creacion de un producto mediante el uso del patron Repository

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  // Buscar mas informacion sobre el patron repositorio
+  constructor(
+    @InjectRepository(Product)
+    private readonly productsRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const product = this.productsRepository.create(createProductDto); // Crea el producto con la "forma" de createProductDto (no hay coneccion con la db)
+      await this.productsRepository.save(product); // guarda el producto en la base de datos (se conecta con la db)
+      return product;
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException('Ayuda!');
+    }
   }
 
   findAll() {
