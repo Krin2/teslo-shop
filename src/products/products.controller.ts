d766fafb3d13fc1778e285c7083a37218aa0dd86
Marcos Oscar Frigerio <marcos.frigerio@intive.com>
2022-09-21 13:30:49 -0300

    Actualizacion de productos usando @BeforeUpdate en la entity

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -12,7 +12,6 @@ import {
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { IsUUID } from 'class-validator';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Controller('products')
@@ -36,8 +35,11 @@ export class ProductsController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(+id, updateProductDto);
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    return this.productsService.update(id, updateProductDto);
   }
 
   @Delete(':id')
