681b2a5e09b29c3eb688a36b01b004b2fc6eb08f
Marcos Oscar Frigerio <marcos.frigerio@intive.com>
2022-09-23 15:42:49 -0300

    Vinculo entre entidades, @OneToMany y @ManyToOne

--- src/products/entities/product-image.entity.ts
+++ src/products/entities/product-image.entity.ts
@@ -3,17 +3,21 @@ import { Product } from './';
 
 @Entity()
 export class ProductImage {
-  ////////////////////////////////
-
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column('text')
   url: string;
 
-  @ManyToOne(
-    () => Product, //relaciona esta variable con la clase Product
-    (product) => product.images, //relaciona a las imagenes de los productos con esta variable
-  )
+  // Describe la relacion "muchos a uno" entre el ProductImage y Product.
+  // El primer termino vincula product en el ProdutImage con Product.
+  // El segundo termino indica la relacion inversa entre Product y ProductImage.
+  // vincula al product aca con el array de imagenes en Product.
+  // Notar que el producto no es opcional, ya que debe estar relacionado a algun producto.
+  // Notar que no se decoro como @Column porque es una referencia a otra tabla y no una columna en si de la entidad
+  @ManyToOne(() => Product, (product) => product.images, {
+    onDelete: 'CASCADE',
+    orphanedRowAction: 'delete',
+  })
   product: Product;
 }
