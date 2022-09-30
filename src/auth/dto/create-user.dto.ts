c34468f5d3bdd6459606d34330b65c386f89bae0
Marcos Oscar Frigerio <marcos.frigerio@intive.com>
2022-09-30 15:31:06 -0300

    Agregado de restricciones de autenticacion y autorizacion a las rutas

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,5 +1,7 @@
 import {
+  IsArray,
   IsEmail,
+  IsIn,
   IsString,
   Matches,
   MaxLength,
@@ -25,4 +27,7 @@ export class CreateUserDto {
   @IsString()
   @MinLength(1)
   fullName: string;
+
+  @IsArray()
+  roles: string[];
 }
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -12,29 +12,36 @@ import {
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { PaginationDto } from '../common/dto/pagination.dto';
+import { Auth } from '../auth/decorators';
+import { ValidRoles } from '../auth/interfaces/valid-roles.interfaces';
 
 @Controller('products')
+// @Auth() // al ponerlo aca implica que cualquiera que este autenticado puede acceder a las rutas esta clase (sin importar el role)
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @Auth(ValidRoles.admin) // solo los admin pueden crear productos
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
   @Get()
+  // no hay restriccion de acceso para ver los productos
   findAll(@Query() paginationDto: PaginationDto) {
     console.log(paginationDto);
     return this.productsService.findAll(paginationDto);
   }
 
   @Get(':term')
+  // no hay restriccion de acceso para ver los productos
   findOne(@Param('term') term: string) {
     return this.productsService.findOnePlain(term);
   }
 
   @Patch(':id')
+  @Auth(ValidRoles.admin) // solo los admin pueden actualizar los productos
   update(
     @Param('id', ParseUUIDPipe) id: string,
     @Body() updateProductDto: UpdateProductDto,
@@ -43,6 +50,7 @@ export class ProductsController {
   }
 
   @Delete(':id')
+  @Auth(ValidRoles.admin) // solo los admin pueden borrar los productos
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.productsService.remove(id);
   }
