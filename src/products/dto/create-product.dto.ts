4901e9745d8b52b08b8a9634e10b9f5bd987ea7b
Marcos Oscar Frigerio <marcos.frigerio@intive.com>
2022-10-03 11:25:42 -0300

    Documentacion de los DTOs de creacion y de actualizacion mediante ApiProperty

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsArray,
   IsIn,
@@ -11,41 +12,56 @@ import {
 } from 'class-validator';
 
 export class CreateProductDto {
+  // Al cargar el @ApiProperty en el dto, el cambio se ve reflejado en la documentacion agregandose en los esquemas al final del documento.
+  // Tambien se le agrega un asterisco a los campos que son obligatorios y se muestra en el Example value
+  @ApiProperty({
+    description: 'Product title (unique)',
+    nullable: false,
+    minLength: 1,
+  })
   @IsString()
   @MinLength(1)
   title: string;
 
+  @ApiProperty()
   @IsNumber()
   @IsPositive()
   @IsOptional()
   price?: number;
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
   description?: string;
 
+  @ApiProperty()
   @IsString()
   @IsLowercase()
   @IsOptional()
   slug?: string;
 
+  @ApiProperty()
   @IsInt()
   @IsPositive()
   @IsOptional()
   stock?: number;
 
+  @ApiProperty()
   @IsString({ each: true })
   @IsArray()
   sizes: string[];
 
+  @ApiProperty()
   @IsIn(['men', 'women', 'kid', 'unisex'])
   gender: string;
 
+  @ApiProperty()
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
   tags: string[];
 
+  @ApiProperty()
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,4 +1,8 @@
-import { PartialType } from '@nestjs/mapped-types';
+// import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateProductDto } from './create-product.dto';
 
+// El PartialType importado de mapped-type no me ayuda a generar la documentacion, por lo que se importa de swagger.
+// no hace falta ningun decorador para que tome los cambios el documento.
+// en el documento se puede ver que los campos son los mismos que en el create, pero sin el asterisco de requerido
 export class UpdateProductDto extends PartialType(CreateProductDto) {}
