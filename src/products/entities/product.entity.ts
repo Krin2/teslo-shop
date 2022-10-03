600845d79dd8e0d28cc4ad61065e404e7ff4d9bd
Marcos Oscar Frigerio <marcos.frigerio@intive.com>
2022-10-03 11:01:30 -0300

    Uso de ApiProperty para documentar entidades y dar ejemplos

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { User } from '../../auth/entities/user.entity';
 import {
   BeforeInsert,
@@ -15,46 +16,89 @@ import { ProductImage } from './';
 // entre las opciones que hay, el name le da el nombre a la tabla en la base de datos
 @Entity({ name: 'products' })
 export class Product {
+  ////////////////////////////////
+
+  @ApiProperty({
+    example: '52c212e7-745d-4787-92f3-6d84e220e53d',
+    description: 'Product ID',
+    uniqueItems: true,
+  }) // Esto le indica al documento que tiene que agregar este campo en el Example value
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   // cuando creamos una columna, le podemos especificar el tipo de contenido que tendra.
   // Hay una lista de estos tipos, pero no todos los tipos son aceptados por la base de datos (por ej postgres aca)
   // Luego del tipo de datos, se le puede asignar propiedades de verificacion, como el ser elemento único
+  @ApiProperty({
+    example: 'T-shirt Teslo',
+    description: 'Product title',
+    uniqueItems: true,
+  })
   @Column('text', {
     unique: true,
   })
   title: string;
 
+  @ApiProperty({
+    example: '0',
+    description: 'Product price',
+  })
   @Column('float', {
     default: 0,
   }) // prestar atencion que si se elige number como tipo de dato, la db no acepta este tipo usando postgres
   price: number;
 
+  @ApiProperty({
+    example: 'Men t-shirts',
+    description: 'Product description',
+    default: null,
+  })
   @Column({
     type: 'text',
     nullable: true,
   })
   description: string;
 
+  @ApiProperty({
+    example: 't_shirt_teslo',
+    description: 'Product slug -for SEO',
+    uniqueItems: true,
+  })
   @Column('text', {
     unique: true,
   })
   slug: string;
 
+  @ApiProperty({
+    example: 10,
+    description: 'Product stock',
+    default: 0,
+  })
   @Column('int', {
     default: 0,
   })
   stock: number;
 
+  @ApiProperty({
+    example: ['XS', ' S', 'M', 'L', 'XL', 'XXL'],
+    description: 'Product available sizes',
+  })
   @Column('text', {
     array: true,
   })
   sizes: string[];
 
+  @ApiProperty({
+    example: 'women',
+    description: 'Product gender',
+  })
   @Column('text')
   gender: string;
 
+  @ApiProperty({
+    example: ['tshirt', 'women'],
+    description: 'Product tags',
+  })
   @Column('text', {
     array: true,
     default: [],
@@ -68,6 +112,10 @@ export class Product {
   // Opcionales: cascade. Se usará para hacer cambios que afecten a ambas tablas
   // Notar que las imagenes son opcionales y vienen en un array de tipo ProductImage.
   // Noter que no se decoro con @Column porque es una referencia a otra tabla y no una columna en si de la entidad
+  @ApiProperty({
+    example: ['1740280-00-A_0_2000.jpg', '1740280-00-A_1.jpg'],
+    description: 'Product images codes',
+  })
   @OneToMany(() => ProductImage, (productImage) => productImage.product, {
     cascade: true,
     eager: true, // ver documentacion de typeorm. Hace que el findOne tome las relaciones al usar find*. Para query no sirve
