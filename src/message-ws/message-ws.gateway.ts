57443cca57a61755ae9c69d2fe65ba7929f86372
Marcos Oscar Frigerio <marcos.frigerio@intive.com>
2022-10-03 14:46:41 -0300

    Manejo de los eventos de conexion y desconexion del lado del servidor

--- src/message-ws/message-ws.gateway.ts
+++ src/message-ws/message-ws.gateway.ts
@@ -1,10 +1,26 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { Socket } from 'socket.io';
 import { MessageWsService } from './message-ws.service';
 
 // En este punto la conexion ya esta andando. se puede verificar obteniendo una respuesta de:
 // localhost:3000/socket.io/socket.io.js
 // La cual es una direccion reservada y contiene toda la documentacion necesaria para realizar la conexion.
 @WebSocketGateway({ cors: true })
-export class MessageWsGateway {
+export class MessageWsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
   constructor(private readonly messageWsService: MessageWsService) {}
+
+  // estas interfaces (provenientes de: OnGatewayConnection y OnGatewayDisconnect) me permiten saber cuando alguien se conecta o desconecta del namespace
+  handleConnection(client: Socket) {
+    // cada socket tiene un id unico. El mismo cambiara cuando el cliente se desconecte y vuelva a conectarse
+    console.log('Cliente conectado', client.id);
+  }
+  handleDisconnect(client: Socket) {
+    console.log('Cliente desconectado', client.id);
+  }
 }
